9686ed515580080e94767620c6cc656f7bf850e6
aryan malik <maddenn.f.l.2500@gmail.com>
2025-09-08 09:05:14 -0400

    added_stock_data

---
+++ optimization.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+def optimize_vector_sum(vector_library, target, error_bound = 1e-5):
+    error = target
+    vector_sum = []
+
+    vector_library_copy = vector_library.copy()
+
+    while vector_library_copy.any() and np.linalg.norm(error) > error_bound:
+        alignments = vector_library_copy @ error
+        most_aligned_idx = np.argmax(abs(alignments))
+        most_aligned_vector = vector_library[most_aligned_idx]
+
+        scaling_coeff = np.dot(most_aligned_vector, error) / np.dot(most_aligned_vector, most_aligned_vector)
+        vector_sum.append((most_aligned_idx, scaling_coeff))
+
+        error = error - scaling_coeff * most_aligned_vector
+        vector_library_copy[most_aligned_idx] = np.zeros_like(most_aligned_vector)
+
+    final_vector = np.zeros_like(error)
+    for vector_idx, coeff in vector_sum:
+        final_vector += coeff * vector_library[vector_idx]
+
+    return vector_sum, final_vector, error
+
+def optimize_constrainted_vector_sum(vector_library, target, covariance_matrix, idio_matrix, error_bound = 1e-5, variance_bias = .75):
+    error = np.array(target)
+    vector_sum = np.zeros(len(vector_library))
+
+    vector_library_copy = vector_library.copy()
+    c = 0
+
+    while vector_library_copy.any() and np.linalg.norm(error) > (error_bound * np.exp(c * error_bound ** (1/2))):
+        all_coeffs = vector_library_copy @ error / (np.einsum("ij,ij->i", vector_library_copy, vector_library_copy) + 1e-8)
+        possible_weights = np.array([vector_sum for i in all_coeffs]) + np.diag(all_coeffs)
+
+        alignments = -abs(all_coeffs) / (error.T @ error) ** (1/2) * (np.einsum("ij,ij->i", vector_library_copy, vector_library_copy) + 1e-8) ** (1/2)
+        variance = np.diag(possible_weights.T @ vector_library @ covariance_matrix @ (possible_weights.T @ vector_library).T + possible_weights.T @ idio_matrix @ possible_weights) / np.sum(abs(possible_weights), axis = 1)**2
+        
+        bias = variance_bias * np.exp(-c / len(vector_sum))
+        loss = alignments * (1 - bias) + variance * bias
+
+        best_idx = np.argmin(loss)
+
+        best_vector = vector_library[best_idx]
+        scaling_coeff = all_coeffs[best_idx]
+
+        vector_sum[best_idx] += scaling_coeff
+        error = error - scaling_coeff * best_vector
+
+        c += 1
+
+    final_vector = vector_sum.T @ vector_library
+    final_variance = vector_sum.T @ vector_library @ covariance_matrix @ (vector_sum.T @ vector_library).T + vector_sum.T @ idio_matrix @ vector_sum / np.sum(abs(possible_weights))**2
+
+    return vector_sum / np.sum(abs(vector_sum)), final_vector, error, final_variance / np.sum(abs(vector_sum)) ** 2, c
---
+++ stock_work.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import requests
+import yfinance as yf
+
+def get_data(ticker_list, interval = None, start = None, end = None):
+    if interval and start and end:
+        data = yf.download(ticker_list, interval = interval, start = start, end = end, group_by = "ticker")
+    else:
+        data = yf.download(ticker_list, period = "max", group_by = "ticker")
+
+    closes = data.loc[:, (slice(None), "Close")]
+    closes.columns = [ticker for ticker, _ in closes.columns]
+
+    volumes = data.loc[:, (slice(None), "Volume")]
+    volumes.columns = [ticker for ticker, _ in volumes.columns]
+
+    return closes, volumes
+
+def get_factors(interval = None, start = None, end = None):
+    """
+    XLC: Communication Services
+    XLY: Consumer Discretionary
+    XLP: Consumer Staples
+    XLE: Energy
+    XLF: Financials
+    XLV: Health Care
+    XLI: Industrials
+    XLB: Materials
+    XLRE: Real Estate
+    XLK: Technology
+    XLU: Utilities
+    """
+    return get_data(['XLC', 'XLY', 'XLP', 'XLE', 'XLF', 'XLV', 'XLI', 'XLB', 'XLRE', 'XLK', 'XLU'])
+
+def regression_results(stocks, factors):
+    print(stocks.shape, factors.shape)
+    stocks = stocks.dropna(axis = 1, how = "all")
+    
+    stock_data_cutoff = stocks.reset_index().isna().any(axis = 1)[::-1].idxmax() - len(stocks) + 1
+    factor_data_cutoff = factors.reset_index().isna().any(axis = 1)[::-1].idxmax() - len(factors) + 1
+    data_cutoff = max(stock_data_cutoff, factor_data_cutoff)
+
+    stocks = stocks.iloc[data_cutoff:].values
+    stocks = (stocks[1:, :] - stocks[:-1, :]) / (stocks[:-1, :] + 1e-8)
+
+    factors = factors.iloc[data_cutoff:].values
+    factors = (factors[1:, :] - factors[:-1, :]) / (factors[:-1, :] + 1e-8)
+    factors = np.hstack((np.ones((len(factors), 1)), factors))
+
+    print(stocks.shape, factors.shape)
+
+    loadings = np.linalg.inv(factors.T @ factors) @ factors.T @ stocks
+    resids = stocks - factors @ loadings
+
+    alphas = loadings[0, :]
+    betas = loadings[1:, :]
+    idios = np.diag(np.sum(resids, axis = 0) ** 2 / (len(factors) - len(factors[0]) - 1))
+    covariance = np.cov(factors[:, 1:], rowvar = False)
+
+    return alphas, betas, idios, covariance
+
+def get_sp500_tickers():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://www.google.com/'
+    }
+
+    try:
+        response = requests.post(url, headers = headers)
+    except:
+        return []
+    
+    tables = pd.read_html(response.text, attrs = {"id": "constituents"})
+    sp500_table = tables[0]
+
+    tickers = sp500_table["Symbol"].tolist()
+    return tickers
